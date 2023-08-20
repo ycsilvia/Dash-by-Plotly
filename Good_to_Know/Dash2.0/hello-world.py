095a3abbabbfcc4b37306f5caaea4333bf6fa2b0
Yixuan Chen <yc4153@columbia.edu>
2023-08-19 22:30:42 -0400

    Update hello-world.py

--- Good_to_Know/Dash2.0/hello-world.py
+++ Good_to_Know/Dash2.0/hello-world.py
@@ -2,15 +2,17 @@
 # https://github.com/Coding-with-Adam/Dash-by-Plotly#execute-code-in-browser
 
 from dash import Dash, dcc               # pip install dash
-import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
+# pip install dash-bootstrap-components
+import dash_bootstrap_components as dbc
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-mytext = dcc.Markdown(children="# Hello World - let's build web apps in Python!")
+mytext = dcc.Markdown(
+    children="# Hello World - let's build web apps in Python!")
 
 # Customize your own Layout
 app.layout = dbc.Container([mytext])
 
 # Run app
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run_server(port=8051)
